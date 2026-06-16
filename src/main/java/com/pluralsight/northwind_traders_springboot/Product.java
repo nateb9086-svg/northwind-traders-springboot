6f03c643a774e7429a03753febe8abf0acadd43b
natefrezzell <nateb9086@gmail.com>
2026-06-16 10:52:12 -0400

    New classes

---
+++ src/main/java/com/pluralsight/northwind_traders_springboot/Product.java
@@ -0,0 +1,9 @@
+package com.pluralsight.northwind_traders_springboot;
+
+public class Product {
+
+    int productId;
+    String name;
+    String category;
+    double price;
+}
---
+++ src/main/java/com/pluralsight/northwind_traders_springboot/ProductRepository.java
@@ -0,0 +1,4 @@
+package com.pluralsight.northwind_traders_springboot;
+
+public interface ProductRepository extends JpaRepository{
+}
